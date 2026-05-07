0e172a6ecb4204ae828fc9c90282d020f87fe6a0
hoeyoon <hoeiyoon57@gmail.com>
2026-05-08 01:52:21 +0900

    1주차 1440 정회윤

---
+++ Week01/1440/hoeyoon.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d: ", i + 1);
+        for(int j = 0; j < n; j++){
+            if(i != j){
+                if(arr[i] < arr[j]){
+                    printf("< ");
+                }
+                else if(arr[i] > arr[j]){
+                    printf("> ");
+                }
+                else if(arr[i] == arr[j]){
+                    printf("= ");
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
