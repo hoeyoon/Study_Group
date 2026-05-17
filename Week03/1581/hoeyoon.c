ac82fb1ebd1a746c56669fd4dfcec0f664ade7f9
hoeyoon <hoeiyoon57@gmail.com>
2026-05-18 00:12:07 +0900

    3주차 1581 정회윤

---
+++ Week03/1581/hoeyoon.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void myswap(int *a, int *b){
+    if(*a > *b){
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    myswap(&a, &b);
+    printf("%d %d", a, b);
+}
